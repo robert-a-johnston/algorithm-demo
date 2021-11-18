47f0ba106408899376ce2065d838ec261eea2ecd
ar[arnau321 <arnau123@att.net>
2021-11-18 13:15:18 -0500

    Fix some display bugs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, {Fragment} from 'react';
 import { Routes, Route } from 'react-router-dom'
 import './App.css';
-import Header from './components/Header/Header';
+import Header from './components/Header/Header'
 import SortDisplay from './components/sort-display/SortDisplay';
 
 
 function App() {
   return (
     <div className="App">
-      hello
       <Fragment>
         <Header/>
+        <main>
         <Routes>
           <Route
           path='/sort'
             element={<SortDisplay/>}/>
         </Routes>
+        </main>
       </Fragment>
     </div>
   );
