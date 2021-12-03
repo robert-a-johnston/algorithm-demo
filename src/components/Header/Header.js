4d7da490073470b834e1730b973e8ed216e32404
ar[arnau321 <arnau123@att.net>
2021-12-03 12:24:46 -0500

    Remove bug fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,10 +12,10 @@ const Header = () => (
         <Nav>
           <NavLink className="nav-link" to="/">Home</NavLink>
           <NavLink className="nav-link" to="/sort">Sorter</NavLink>
-          <NavLink className="nav-link" to="/path">
-            <span className="span-link" onClick= {
+          <NavLink className="nav-link" to="/path">PathFinder
+            {/* <span className="span-link" onClick= {
               e=> { e.stopPropagation()}
-            }>PathFinder</span>
+            }>PathFinder</span> */}
           </NavLink>
         </Nav>
         
